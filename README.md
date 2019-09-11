4b708fdf2e44eab33d51ba7be642427e9a72602d
leornado <leornado@126.com>
2019-09-11 13:39:17 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# toggle-case package
+
+Toggle case with the same command.
+
+```json
+"ctrl-shift-X": "toggle-case:toggle"
+```
---
+++ spec/switch-case-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SwitchCaseView from '../lib/switch-case-view';
+
+describe('SwitchCaseView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
